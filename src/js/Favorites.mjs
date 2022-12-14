61032bac78067924d8abf60d0d674a466f569b52
joaquin solis <sol19008@byui.edu>
2022-12-14 00:48:33 -0300

    fix api issue

--- src/js/Favorites.mjs
+++ src/js/Favorites.mjs
@@ -1,7 +1,7 @@
 import { renderListWithTemplate } from "./utils.mjs";
 
 function movieCardTemplate(movie) {
-    return `<li class="product-card">
+  return `<li class="product-card">
       <a href="/movie-page/index.html?movie=${movie.id}">
       <img
       src="http://image.tmdb.org/t/p/w500${movie.poster_path}"
@@ -9,38 +9,38 @@ function movieCardTemplate(movie) {
     />
       <h1 class="card__brand">${movie.title}</h1></a>
     </li>`;
+}
+
+export default class FavoritesList {
+  constructor(dataSource, listElement) {
+    // We passed in this information to make our class as reusable as possible.
+    // Being able to define these things when we use the class will make it very flexible
+    this.favIds = [];
+    this.favMovies = [];
+    // this.idTitle = idTitle;
+    this.dataSource = dataSource;
+    this.listElement = listElement;
   }
-  
-  export default class FavoritesList {
-    constructor(dataSource, listElement) {
-      // We passed in this information to make our class as reusable as possible.
-      // Being able to define these things when we use the class will make it very flexible
-      this.favIds = [];
-      this.favMovies = {};
-      // this.idTitle = idTitle;
-      this.dataSource = dataSource;
-      this.listElement = listElement;
+  async init() {
+    this.getFavIds();
+    for (let i = 0; i < this.favIds.length; i++) {
+      console.log(this.favIds[i]);
+      const movie = await this.dataSource.getMovieById(this.favIds[i]);
+      this.favMovies.push(movie);
     }
-    async init() {
-      this.getFavIds();
-      console.log(this.favIds);
-      for (let id in this.favIds) {
-        const movie = await this.dataSource.getMovieById(id);
-        this.favMovies.push(movie);
-        console.log(movie.results);
-      }
-      // our dataSource will return a Promise...so we can use await to resolve it.
-      // const list = await this.dataSource.getMovieById(this.favMovies);
-      // console.log(list.results);
-      // // render the list
-      // this.renderList(list.results);
-      this.renderList(this.favMovies.results);
-    }
-    // render after doing the first stretch
-    renderList(list) {
-      renderListWithTemplate(movieCardTemplate, this.listElement, list);
-    }
-    getFavIds() {
-      this.favIds = localStorage.getItem("fav-movies").split(",");
-    }
+    console.log(this.favMovies);
+    // our dataSource will return a Promise...so we can use await to resolve it.
+    // const list = await this.dataSource.getMovieById(this.favMovies);
+    // console.log(list.results);
+    // // render the list
+    // this.renderList(list.results);
+    this.renderList(this.favMovies);
   }
+  // render after doing the first stretch
+  renderList(list) {
+    renderListWithTemplate(movieCardTemplate, this.listElement, list);
+  }
+  getFavIds() {
+    this.favIds = localStorage.getItem("fav-movies").split(",");
+  }
+}
